4a30ecfb458c1aa711dba961b003b8891065f27f
akane9x <akane9x@gmail.com>
2021-09-26 21:16:05 +0700

    asd

--- db.php
+++ db.php
@@ -1,6 +1,38 @@
 <?php
 
-require('vendor/autoload.php');
+
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'kdlhxyz_dich',
+    'username' => 'kdlhxyz_akane9x',
+    'password' => 'lakemanh1310',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+]);
+
+$capsule->setEventDispatcher(new Dispatcher(new Container));
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
+
+function x($a){
+      echo "<pre>";
+      print_r($a);
+      echo "</pre>";
+}
 
 ?>
 
--- func.php
+++ func.php
@@ -1,34 +1,9 @@
 <?php
 
 require_once('vendor/autoload.php');
-//abc
 use Carbon\Carbon;
-
-use Illuminate\Database\Capsule\Manager as Capsule;
-
-use Illuminate\Events\Dispatcher;
-use Illuminate\Container\Container;
-
-$capsule = new Capsule;
-
-$capsule->addConnection([
-    'driver' => 'mysql',
-    'host' => 'localhost',
-    'database' => 'kdlhxyz_dich',
-    'username' => 'kdlhxyz_akane9x',
-    'password' => 'lakemanh1310',
-    'charset' => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix' => '',
-]);
-
-$capsule->setEventDispatcher(new Dispatcher(new Container));
-
-// Make this Capsule instance available globally via static methods... (optional)
-$capsule->setAsGlobal();
-
-// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
-$capsule->bootEloquent();
+//abc
+require_once('db.php');
 
 function x($a){
       echo "<pre>";
